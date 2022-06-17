8e5ac465e13e5c3fac73939e35b8c36a024ba7be
Masoudi Parrot <masoudi@localhost.localdomain>
2022-06-17 18:40:51 -0400

    correct linter errors

--- app.rb
+++ app.rb
@@ -51,5 +51,4 @@ class App
     @rentals.select { |rental| rental.person.id == person_id }
       .map { |rental| print "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}" }
   end
-    
 end
